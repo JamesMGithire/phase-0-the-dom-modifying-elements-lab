fccd57bb627fe0c74b045aad15f33fcf0cbf4096
James Mburu Githire <jamesmburug24@gmail.com>
2022-08-03 18:26:33 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+
+//Make sure you remove the <main> with id 'main'
+let main = document.getElementById('main');
+main.remove();
+
+//Make sure you create an <h1> with id 'victory'
+let newHeader = document.createElement('h1');
+newHeader.id = "victory";
+
+//"the 'newHeader' variable points to a node 'h1#victory', has \"YOUR-NAME is the champion\" inside"
+newHeader.textContent = "YOUR-NAME is the champion";
